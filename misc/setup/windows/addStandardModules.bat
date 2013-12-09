2e37df1ebdf894b8b332964f1dfce4c21abd406a
Frieder Pankratz <pankratz@in.tum.de>
2013-12-10 00:06:29 +0100

    - renamed repository for direct show framegrabber

--- misc/setup/windows/addStandardModules.bat
+++ misc/setup/windows/addStandardModules.bat
@@ -5,4 +5,4 @@ git submodule add https://github.com/Ubitrack/utvisioncomponents.git modules/utv
 git submodule add https://github.com/Ubitrack/utdataflow.git modules/utdataflow
 git submodule add https://github.com/Ubitrack/utfacade.git modules/utfacade
 git submodule add https://github.com/Ubitrack/utvisualization.git modules/utvisualization
-git submodule add https://github.com/Ubitrack/mswindows.git modules/mswindows
+git submodule add https://github.com/Ubitrack/directshow.git modules/directshow
